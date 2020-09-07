36d7732aed10d8093dcb36d6f7d606beceb418a0
Nikita Zhukov <xzhuko01@vutbr.cz>
2020-09-07 23:05:54 +0200

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# script_to_compess_logs
+# Script to compress logs 
+
+Supported OS
+```
+ Mac Os
+```
+Description of the script
+```text
+NAME - stands for the name of the output log file
+
+SCRIPT - script that is going to be executed 
+
+COMMAND - complex script that encapsulates utility called crontab for scheduling an executing
+```
+To run
+```text
+python3 scheduler.py
+```
+Useful links
+```text
+https://crontab.guru
+```
--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,32 @@
 import os
 from datetime import datetime
 
+# Global variables
 NAME = "LOGS" + str(datetime.timestamp(datetime.now())).replace('.', '')
 SCRIPT = "tar -czf /var/log/" + NAME + " /var/log"
-COMMAND = "(crontab -l ; echo '* * * * * " + SCRIPT + "')| crontab -"
-os.system(command=COMMAND)
 
-print(COMMAND)
+
+def init_cmd(**kwargs):
+    '''
+
+    :param kwargs: contains all needed parameters to execute program
+    :return: initialized command
+    '''
+
+    time = '* * * * * '
+    script = 'echo "Hello World!" '
+
+
+    for k, v in kwargs.items():
+
+        if k == 'time':
+            time = v
+
+        if k == 'script':
+            script = v
+
+    return "(crontab -l ; echo '" + time + script + "') | crontab -"
+
+
+if __name__ == "__main__":
+    os.system(init_cmd(time='0 0 1 */1 * ', script=SCRIPT))
